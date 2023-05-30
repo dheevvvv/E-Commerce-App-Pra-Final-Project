194d803d5228015e72f1a4e7562fa0f5e5fa885d
dhevadayatvitoindraj <dhevadayatvitoindraj@mail.ugm.ac.id>
2023-05-30 10:27:08 +0700

    adding model response favourite, Cart, and history transaction

---
+++ app/src/main/java/com/pucuk/e_commerce_app_pra_final_project/model/DataCartResponseItem.kt
@@ -0,0 +1,21 @@
+package com.pucuk.e_commerce_app_pra_final_project.model
+
+
+import com.google.gson.annotations.SerializedName
+
+data class DataCartResponseItem(
+    @SerializedName("createdAt")
+    val createdAt: String,
+    @SerializedName("description")
+    val description: String,
+    @SerializedName("id_cart")
+    val idCart: String,
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("price")
+    val price: String,
+    @SerializedName("product_image")
+    val productImage: String,
+    @SerializedName("userId")
+    val userId: String
+)
---
+++ app/src/main/java/com/pucuk/e_commerce_app_pra_final_project/model/DataFavouriteResponse.kt
@@ -0,0 +1,6 @@
+package com.pucuk.e_commerce_app_pra_final_project.model
+
+
+import com.google.gson.annotations.SerializedName
+
+class DataFavouriteResponse : ArrayList<DataFavouriteResponseItem>()
