2832ceb3b4e68da524d396cd5f7af35ae7f531f1
Pucukio <pucuk4us@gmail.com>
2023-05-31 09:52:23 +0700

    Update Localization En-US

---
+++ app/src/main/java/com/pucuk/e_commerce_app_pra_final_project/view/ui/FavoriteFragment.kt
@@ -0,0 +1,33 @@
+package com.pucuk.e_commerce_app_pra_final_project.view.ui
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.pucuk.e_commerce_app_pra_final_project.viewmodel.FavoriteViewModel
+import com.pucuk.e_commerce_app_pra_final_project.R
+
+class FavoriteFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = FavoriteFragment()
+    }
+
+    private lateinit var viewModel: FavoriteViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_favorite, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(FavoriteViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
