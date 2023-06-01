0ecdffa74ae44c2117ee27123e7c78511655ee61
Pucukio <pucuk4us@gmail.com>
2023-06-02 00:32:34 +0700

    Update Detail Product Layout

---
+++ app/src/main/java/com/pucuk/e_commerce_app_pra_final_project/viewmodel/DetailProductViewModel.kt
@@ -0,0 +1,7 @@
+package com.pucuk.e_commerce_app_pra_final_project.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class DetailProductViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
