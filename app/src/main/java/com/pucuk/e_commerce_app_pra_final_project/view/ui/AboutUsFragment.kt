7dc0f84e8153f03b797ff5ab063f5f202b596eba
Pucukio <pucuk4us@gmail.com>
2023-06-02 01:35:12 +0700

    Update Navigate for Feedback and About Fragment

--- app/src/main/java/com/pucuk/e_commerce_app_pra_final_project/view/ui/AboutUsFragment.kt
+++ app/src/main/java/com/pucuk/e_commerce_app_pra_final_project/view/ui/AboutUsFragment.kt
@@ -5,12 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.pucuk.e_commerce_app_pra_final_project.R
 import com.pucuk.e_commerce_app_pra_final_project.databinding.FragmentAboutUsBinding
 
 class AboutUsFragment : Fragment() {
 
-    private var _binding: FragmentAboutUsBinding? = null
+    lateinit var binding: FragmentAboutUsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,11 +19,10 @@ class AboutUsFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_about_us, container, false)
-    }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+        binding.btnBack.setOnClickListener {
+            findNavController().navigate(R.id.action_aboutUsFragment_to_accountFragment)
+        }
     }
 
 }
--- app/src/main/java/com/pucuk/e_commerce_app_pra_final_project/view/ui/DetailNewsFragment.kt
+++ app/src/main/java/com/pucuk/e_commerce_app_pra_final_project/view/ui/DetailNewsFragment.kt
@@ -42,19 +42,19 @@ class DetailNewsFragment : Fragment() {
         binding.bottomNavigation.setOnNavigationItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.home -> {
-                    findNavController().navigate(R.id.action_newsFragment_to_homeFragment)
+                    findNavController().navigate(R.id.action_detailNewsFragment_to_homeFragment)
                     true
                 }
                 R.id.favorite -> {
-                    findNavController().navigate(R.id.action_newsFragment_to_favoriteFragment)
+                    findNavController().navigate(R.id.action_detailNewsFragment_to_favoriteFragment)
                     true
                 }
                 R.id.cart -> {
-                    findNavController().navigate(R.id.action_newsFragment_to_keranjangFragment)
+                    findNavController().navigate(R.id.action_detailNewsFragment_to_keranjangFragment)
                     true
                 }
                 R.id.account -> {
-                    findNavController().navigate(R.id.action_newsFragment_to_accountFragment)
+                    findNavController().navigate(R.id.action_detailNewsFragment_to_accountFragment)
                     true
                 }
 
