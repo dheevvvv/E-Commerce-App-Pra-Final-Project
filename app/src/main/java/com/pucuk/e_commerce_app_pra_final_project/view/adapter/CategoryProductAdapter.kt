9863df440eed89b7888ecd9962aa791e119ae915
dhevadayatvitoindraj <dhevadayatvitoindraj@mail.ugm.ac.id>
2023-06-01 23:47:44 +0700

    adding Category Product Adapter

---
+++ app/src/main/java/com/pucuk/e_commerce_app_pra_final_project/view/adapter/CategoryProductAdapter.kt
@@ -0,0 +1,34 @@
+package com.pucuk.e_commerce_app_pra_final_project.view.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.pucuk.e_commerce_app_pra_final_project.databinding.ItemHistoryTransactionBinding
+import com.pucuk.e_commerce_app_pra_final_project.databinding.ItemProductCategoryBinding
+import com.pucuk.e_commerce_app_pra_final_project.model.category_product_response.DataCategoryProductResponseItem
+import com.pucuk.e_commerce_app_pra_final_project.model.transaction_history_response.DataHistoryTransactionResponseItem
+
+class CategoryProductAdapter (private var listCategoryProduct: List<DataCategoryProductResponseItem>)
+    : RecyclerView.Adapter<CategoryProductAdapter.ViewHolder>() {
+
+    class ViewHolder(var binding: ItemProductCategoryBinding): RecyclerView.ViewHolder(binding.root) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = ItemProductCategoryBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return listCategoryProduct.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int){
+        val categoryProductItem = listCategoryProduct[position]
+        Glide.with(holder.itemView).load(categoryProductItem.image).into(holder.binding.ivImgProduct)
+        holder.binding.tvNamaCategoryProduct.text = categoryProductItem.name
+
+    }
+}
