eeb10e905164410fb76c8b9d609bc42fa0cf36e1
dhevadayatvitoindraj <dhevadayatvitoindraj@mail.ugm.ac.id>
2023-06-01 16:36:38 +0700

    modify mainActivity tapi masih belom coba gess tolong benerin biar bottom nav nya bisa kepake

--- app/src/main/java/com/pucuk/e_commerce_app_pra_final_project/MainActivity.kt
+++ app/src/main/java/com/pucuk/e_commerce_app_pra_final_project/MainActivity.kt
@@ -1,13 +1,46 @@
 package com.pucuk.e_commerce_app_pra_final_project
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+//    private lateinit var  navController: NavController
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        navController = findNavController(androidx.navigation.fragment.R.id.nav_host_fragment_container)
+//        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+//        bottomNavigationView.setupWithNavController(navController)
+//
+//        bottomNavigationView.setOnNavigationItemSelectedListener {item ->
+//            when (item.itemId){
+//                R.id.home -> {
+//                    navController.navigate(R.id.homeFragment)
+//                    true
+//                }
+//                R.id.cart -> {
+//                    navController.navigate(R.id.keranjangFragment)
+//                    true
+//                }
+//                R.id.news -> {
+//                    navController.navigate(R.id.newsFragment)
+//                    true
+//                }
+//                R.id.favorite -> {
+//                    navController.navigate(R.id.wishlistFragment)
+//                    true
+//                }
+//                else -> false
+//            }
+//        }
     }
 }
